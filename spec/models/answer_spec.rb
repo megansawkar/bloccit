02ced11120f2345b01c3bdae412694b8bfbfb6a1
Megan Sawkar <mwelch122@gmail.com>
2016-07-27 18:58:04 -0700

    Assignment 28: Updated Final

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: "True or False") }
+  let(:question) { Question.create!(title: "New Post Title", body: "New Post Body", resolved: false) }
   let(:answer) { Answer.create!(body: 'Comment Body', question: question) }
 
   describe "attributes" do
