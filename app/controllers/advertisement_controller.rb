ba8d8a06d2895513d3f41dbafed4a267fcc8671b
Megan Sawkar <mwelch122@gmail.com>
2016-08-02 13:03:03 -0700

    Assignment 32: First Commit

---
+++ app/controllers/advertisement_controller.rb
@@ -0,0 +1,15 @@
+class AdvertisementController < ApplicationController
+  def index
+    @advertisements = Advertisement.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
