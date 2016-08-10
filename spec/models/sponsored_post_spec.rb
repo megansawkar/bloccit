015a823e0466313234dd53bd79e16079b0035cce
Megan Sawkar <mwelch122@gmail.com>
2016-08-09 20:37:47 -0700

    Assignment 34: Second Commit

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPost, type: :model do
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
-  let(:price) { RandomData.random_word }
-  let(:topic) { Topic.create!(title: title, body: body) }
-end
+  let(:price) { RandomData.random_integer }
+  let(:topic) { Topic.create!(name: name, description: description) }
+  let(:sponsored_post) { topic.sponsored_posts.create!(title: title, body: body, price: price) }
 
-  it { is_expected.to have_many(:posts) }
+  it { is_expected.to belong_to(:topic) }
 
   describe "attributes" do
-    it "has title, body and price attributes" do
-      expect(topic).to have_attributes(title: title, body: body, price: price)
+    it "has title and body attributes" do
+      expect(sponsored_post).to have_attributes(title: title, body: body, price: price)
     end
   end
+end
