3a1ccdc267fd757742f8f2b0efc2de7c04980832
Megan Sawkar <mwelch122@gmail.com>
2016-08-18 21:17:29 -0700

    Checkpoint 40: Complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :posts 
+  has_many :posts
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -10,4 +11,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false },length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  enum role: [:member, :admin]
 end
