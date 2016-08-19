1b09499242f90235883e3dda2d965aa35decee34
Megan Sawkar <mwelch122@gmail.com>
2016-08-19 12:45:24 -0700

    Assignment 40: First Commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -63,9 +63,19 @@ class TopicsController < ApplicationController
   end
 
   def authorize_user
-    unless current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
-      redirect_to topics_path
-    end 
+#    unless current_user.admin? || current_user.moderator?
+#      flash[:alert] = "You must be an admin to do that."
+#      redirect_to topics_path
+#     end
+
+     if user.role.admin?
+       can :new, :create, :edit, :update, :destroy, Topic
+     elsif user.role.moderator?
+       can :edit, :update, Topic
+     else
+       flash[:alert] = "You must be an admin to do that."
+       redirect_to topics_path
+     end
+
   end
 end
