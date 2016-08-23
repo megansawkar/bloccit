023fe596ecffff8d4d6a8b1849a93d2174e96c32
Megan Sawkar <mwelch122@gmail.com>
2016-08-23 13:43:44 -0700

    Assignment 41: Complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -66,6 +66,6 @@ class TopicsController < ApplicationController
     unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
       redirect_to topics_path
-    end 
+    end
   end
 end
