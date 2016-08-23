131a2c1e38f59a2e005a606035d569b35b4d5826
Megan Sawkar <mwelch122@gmail.com>
2016-08-23 10:10:12 -0700

    Assignment 40: Updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
-  #before_action :authorize_moderator, only: [:create, :new, :destroy]
+  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_member, only: [:create, :new, :edit, :update, :destroy]
+  before_action :authorize_admin, only: :destroy
+#  before_action :authorize_admin, only: [:create, :new, :update, :edit, :destroy]
+  before_action :authorize_moderator, only: [:create, :new, :edit, :update]
 
   def create
     @topic = Topic.find(params[:topic_id])
@@ -62,20 +65,41 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-  def authorize_user
-    post = Post.find(params[:id])
 
-    unless current_user == post.user || current_user.admin?
+  def authorize_user
+#    post = Post.find(params[:id])
+    return unless current_user.member?
+#    unless current_user.admin?
       flash[:alert] = "You must be an admin to do that."
-      redirect_to [post.topic, post]
-    end 
+      redirect_to new_session_path  #topics_path
   end
 
-#  def authorize_moderator
+  def authorize_admin
 #    post = Post.find(params[:id])
-#
-#    return unless current_user.moderator?
-#      flash[:alert] = "You must be an admin to do that."
-#      redirect to [post.topic, post]
-#  end
+    return unless current_user.admin?
+
+    flash[:alert] = "You must be an admin to do that."
+    redirect_to new_session_path #topics_path
+  end
+
+  def authorize_member
+    post = Post.find(params[:id])
+
+    return unless current_user == post.member #|| current_user.admin? || current_user.moderator?
+    flash[:alert] = "You must be signed in to do that."
+    redirect_to new_session_path
+  end
+
+  def authorize_moderator
+#    post = Post.find(params[:id])
+
+    return unless current_user.moderator?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to new_session_path #[post.topic, post]
+  end
 end
+
+#    unless current_user == post.user || current_user.admin?
+#      flash[:alert] = "You must be an admin to do that."
+#      redirect_to [post.topic, post]
+#  end
