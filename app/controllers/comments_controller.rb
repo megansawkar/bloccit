db38d590443732fba9db3a836d25d8b61eed33c8
Megan Sawkar <mwelch122@gmail.com>
2016-08-29 12:26:00 -0700

    Assignment 42: Still Working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,38 +3,52 @@ class CommentsController < ApplicationController
   before_action :authorize_user, only: [:destroy]
 
   def create
-    @post = Post.find(params[:post_id])
-    comment = @post.comments.new(comment_params)
-    comment.user = current_user
-
-    if comment.save
-      flash[:notice] = "Comment saved successfully."
-      redirect_to [@post.topic, @post]
-    else
-      flash[:alert] = "Comment failed to save."
-      redirect_to [@post.topic, @post]
+    id = params[:post_id] || params[:topic_id]
+    if params[:post_id]
+      @parent = Post.find id
+    elsif params[:topic_id]
+      @parent = Topic.find id
     end
+
+    @comment = @parent.comments.find params[:id]
+    @comment.save
   end
+#    @post = Post.find(params[:post_id])
+#    comment = @post.comments.new(comment_params)
+#    comment.user = current_user
+
+#    if comment.save
+#      flash[:notice] = "Comment saved successfully."
+#      redirect_to [@post.topic, @post]
+#    else
+#      flash[:alert] = "Comment failed to save."
+#      redirect_to [@post.topic, @post]
+#    end
+#  end
 
   def destroy
-     @post = Post.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
+    @parent = params[:post_id] || params[:topic_id]
+    @comment = @parent.comments.new comment_params
+    @comment.destroy
+  end
+#     @post = Post.find(params[:post_id])
+#     comment = @post.comments.find(params[:id])
 
-     if comment.destroy
-       flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@post.topic, @post]
-     else
-       flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
-     end
-   end
+#     if comment.destroy
+#       flash[:notice] = "Comment was deleted successfully."
+#       redirect_to [@post.topic, @post]
+#     else
+#       flash[:alert] = "Comment couldn't be deleted. Try again."
+#       redirect_to [@post.topic, @post]
+#     end
+
 
 
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:x, :y)
   end
 
   def authorize_user
